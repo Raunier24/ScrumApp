84b53736b37575897215938d618d048ff136c2c3
Raunier24 <peplisoft@gmail.com>
2024-09-12 12:22:16 +0200

    branch dev

---
+++ src/main/java/com/scrumapp/scrum/ScrumappApplication.java
@@ -0,0 +1,13 @@
+package com.scrumapp.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrumapp/scrum/ScrumappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrumapp.scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
