629ae0d899a23e5b8d81629a44bcab1a7d49e0b1
Raunier24 <peplisoft@gmail.com>
2024-09-17 11:19:41 +0200

    update AuthController

--- src/main/java/com/scrumapp/scrum/security/JwtRequestFilter.java
+++ src/main/java/com/scrumapp/scrum/security/JwtRequestFilter.java
@@ -1,5 +1,6 @@
 package com.scrumapp.scrum.security;
 
+import com.scrumapp.scrum.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
